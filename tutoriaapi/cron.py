f9655520ff132f15f113796927ba356988260528
Matthew Man <matthewman9639@gmail.com>
2017-11-07 23:52:44 +0800

    cron file updated to pay to tutor during end all sessions

--- tutoriaapi/cron.py
+++ tutoriaapi/cron.py
@@ -9,10 +9,21 @@ class cronJob(CronJobBase):
     code = 'cron.cron_job'    # a unique code
 
     def do(self):
-        print(User.objects.all().count())
-        for tutorial in Tutorial.objects.all():
+        for tutorial in Tutorial.objects.filter(cancelled=False).all():
             print(tutorial.start_time)
             print(tutorial.end_time)
-            print(datetime.now(tz=timezone.utc))
-            print(datetime.now(tz=timezone.utc) > tutorial.start_time)
-            
+            # print(datetime.now(tz=timezone.utc))
+            # print(datetime.now(tz=timezone.utc) > tutorial.end_time)
+        print("Locking all sessions")
+        for tutorial in Tutorial.objects.filter(cancelled=False).filter(start_time__lte=datetime.now(tz=timezone.utc)).filter(locked=False).all():
+            print(tutorial.start_time)
+            print(tutorial.end_time)
+            tutorial.locked=True
+            tutorial.save()
+        print("Locked all sessions")
+        print("Ending all sessions")
+        for tutorial in Tutorial.objects.filter(locked=True).filter(end_time__lte=datetime.now(tz=timezone.utc)).filter(company_to_tutor_transaction__isnull=True).all():
+            print(tutorial.start_time)
+            print(tutorial.end_time)
+            tutorial.pay_to_tutor()
+        print("Ended all sessions")
