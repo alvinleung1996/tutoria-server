f5173b5e8d162e90199ab7d511fb7f79f625a721
alvinleung1996 <alvinleung1996@outlook.com>
2017-11-16 15:37:55 +0800

    Fixed Transaction model bug when handling None input

--- tutoriaapi/models/transaction.py
+++ tutoriaapi/models/transaction.py
@@ -3,7 +3,9 @@ from datetime import datetime, timezone
 
 from django.db import models, transaction
 
-from . import wallet
+from . import wallet, user
+
+from ..api_exception import ApiException
 
 class Transaction(models.Model):
 
@@ -12,12 +14,24 @@ class Transaction(models.Model):
         if time is None:
             time = datetime.now(tz=timezone.utc)
 
-        withdraw_wallet = withdraw if isinstance(withdraw, wallet.Wallet) else withdraw.wallet
-        deposit_wallet = deposit if isinstance(deposit, wallet.Wallet) else deposit.wallet
+        if isinstance(withdraw, user.User):
+            withdraw_wallet = withdraw.wallet
+        else:
+            withdraw_wallet = withdraw
+        
+        if isinstance(deposit, user.User):
+            deposit_wallet = deposit.wallet
+        else:
+            deposit_wallet = deposit
+        
+        if withdraw_wallet is None and deposit_wallet is None:
+            raise ApiException(message='Cannot create transaction with both deposit and withdraw is None')
 
         with transaction.atomic():
-            withdraw_wallet.withdraw(amount)
-            deposit_wallet.deposit(amount)
+            if withdraw_wallet is not None:
+                withdraw_wallet.withdraw(amount)
+            if deposit_wallet is not None:
+                deposit_wallet.deposit(amount)
 
         return cls.objects.create(
             amount = amount,
